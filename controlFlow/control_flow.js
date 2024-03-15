14ea888cb37f3c4eba7f5cf88e1af279640e5011
sanuarKhan <sanuarkhan220@gmail.com>
2024-03-15 13:51:50 -0400

    control-flow done

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,45 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+//check whether user loggrdin or not
+if(isLoggedIn){
+    if(userRole=== 'admin'){
+        userMessage = "Welcome, Admin!";
+    } else{
+        userMessage = "Welcome, Uder!";
+    }
+} else{
+    userMessage = "Please log in to access the system.";
+}
+console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+// check user Type
+switch(userType){
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+console.log("User Category:", userCategory);
